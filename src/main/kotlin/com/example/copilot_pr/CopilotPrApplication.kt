1c3fad66819a5457b8415a88e4b281553561b360
radrien <raphael.adrien@daimlertruck.com>
2026-02-12 16:53:32 +0000

    Initial commit

---
+++ src/main/kotlin/com/example/copilot_pr/CopilotPrApplication.kt
@@ -0,0 +1,11 @@
+package com.example.copilot_pr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CopilotPrApplication
+
+fun main(args: Array<String>) {
+	runApplication<CopilotPrApplication>(*args)
+}
